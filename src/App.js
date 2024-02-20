4fd3747bd1ebfddb4407e382c3f716a1dcb6e1e4
brittajaison <britta@nus.edu.sg>
2024-02-20 20:56:30 +0800

    Change password template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import  Login from './auth/Login';
+import Layout from './pages/Layout'
+import ChangePassword from "./auth/ChangePassword";
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<Layout />}>
+                  <Route index element={<Login />} />
+                  <Route path="changepassword" element={<ChangePassword />} />
+
+              </Route>
+          </Routes>
+      </BrowserRouter>
+
+
+    // <div className="App">
+    //   <Login/>
+    // </div>
   );
 }
 
