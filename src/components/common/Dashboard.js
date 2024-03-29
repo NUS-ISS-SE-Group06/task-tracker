d79669fa57f34a31fe70bf89e66b4869642280d7
William-Dou <xiaolongmamei@gmail.com>
2024-03-29 21:02:02 +0800

    singple page implementation

--- src/components/common/Dashboard.js
+++ src/components/common/Dashboard.js
@@ -1,78 +1,112 @@
 import { Outlet, Link } from "react-router-dom";
 import React from 'react';
+import { useState } from "react";
 import '../../assets/styles/Style.css';
 import { getCookieValue } from '../../services/cookieService';
 import { handleLogout } from '../../services/authService';
 import '../../assets/styles/Dashboard.css'; // Import CSS file for login component stylesß
+import { Table } from "./Table";
+import { Modal } from "./Modal";
+import UserRegistration from '../../userreg/UserRegistration';
 const Dashboard = () => {
+
+    const [modalOpen, setModalOpen] = useState(false);
+    const [rows, setRows] = useState([
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+        { "taskName": "Task 9", "taskCreationDate": "25/03/2024 ", "taskAssignmentDate": "27/03/2024", "taskAssignee": "Yudi", "taskDueDate": "30/03/2024", "status": "Draft" },
+
+
+    ]);
+
+    const [view, setView] = useState('table'); // 'table' is the default view
+
+    const [rowToEdit, setRowToEdit] = useState(null);
+
+    const handleDeleteRow = (targetIndex) => {
+        setRows(rows.filter((_, idx) => idx !== targetIndex));
+    };
+
+    const handleEditRow = (idx) => {
+        setRowToEdit(idx);
+
+        setModalOpen(true);
+    };
+
+    const handleSubmit = (newRow) => {
+        rowToEdit === null
+            ? setRows([...rows, newRow])
+            : setRows(
+                rows.map((currRow, idx) => {
+                    if (idx !== rowToEdit) return currRow;
+
+                    return newRow;
+                })
+            );
+    };
 
     const role = getCookieValue('userRole')
     if (!role) {
         handleLogout();
     }
 
-    // Define the dummy data
-    const data = [
-        // Each object in this array represents a row in the table
-        // Fill in the actual data here
-        { "Task Name": "Task 1", "Task Creation Date": "2024-03-25", "Task Assignment Date": "2024-03-27 ", "Task Assignee": "William", "Task Progress": "50%", "Task Due Date": "2024-03-30 " },
-        { "Task Name": "Task 2", "Task Creation Date": "2024-03-25 ", "Task Assignment Date": "2024-03-27 ", "Task Assignee": "Yudi", "Task Progress": "50%", "Task Due Date": "2024-03-30 " },
-        { "Task Name": "Task 3", "Task Creation Date": "2024-03-25 ", "Task Assignment Date": "2024-03-27 ", "Task Assignee": "Yew Hoon", "Task Progress": "50%", "Task Due Date": "2024-03-30 " },
-
-        { "Task Name": "Task 4", "Task Creation Date": "2024-03-25 ", "Task Assignment Date": "2024-03-27 ", "Task Assignee": "Britta", "Task Progress": "50%", "Task Due Date": "2024-03-30 " },
-
-        { "Task Name": "Task 5", "Task Creation Date": "2024-03-25 ", "Task Assignment Date": "2024-03-27 ", "Task Assignee": "Balaji", "Task Progress": "50%", "Task Due Date": "2024-03-30 " },
-        { "Task Name": "Task 6", "Task Creation Date": "2024-03-25 ", "Task Assignment Date": "2024-03-27 ", "Task Assignee": "Balaji", "Task Progress": "50%", "Task Due Date": "2024-03-30 " },
-        { "Task Name": "Task 7", "Task Creation Date": "2024-03-25 ", "Task Assignment Date": "2024-03-27 ", "Task Assignee": "Balaji", "Task Progress": "50%", "Task Due Date": "2024-03-30 " },
-        { "Task Name": "Task 8", "Task Creation Date": "2024-03-25 ", "Task Assignment Date": "2024-03-27 ", "Task Assignee": "William", "Task Progress": "50%", "Task Due Date": "2024-03-30 " },
-        { "Task Name": "Task 9", "Task Creation Date": "2024-03-25 ", "Task Assignment Date": "2024-03-27 ", "Task Assignee": "Yudi", "Task Progress": "50%", "Task Due Date": "2024-03-30 " },
-        { "Task Name": "Task 10", "Task Creation Date": "2024-03-25 ", "Task Assignment Date": "2024-03-27 ", "Task Assignee": "Britta", "Task Progress": "50%", "Task Due Date": "2024-03-30 " },
-        { "Task Name": "Task 11", "Task Creation Date": "2024-03-25 ", "Task Assignment Date": "2024-03-27 ", "Task Assignee": "Britta", "Task Progress": "50%", "Task Due Date": "2024-03-30 " },
-        { "Task Name": "Task 1", "Task Creation Date": "2024-03-25", "Task Assignment Date": "2024-03-27 ", "Task Assignee": "William", "Task Progress": "50%", "Task Due Date": "2024-03-30 " }
-      
-    ];
 
     return (
-        <>
-
+        <div className="container">
             <div className="header">
                 <h1>{role === 'ROLE_ADMIN' ? 'Welcome to Administrator Task Tracker Dashboard' : 'Welcome to Task Tracker Dashboard'}</h1>
             </div>
-            <div className="dashboard">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Task Name</th>
-                            <th>Task Creation Date</th>
-                            <th>Task Assignment Date</th>
-                            <th>Task Assignee</th>
-                            <th>Task Progress</th>
-                            <th>Task Due Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((row, index) => (
-                            <tr key={index}>
-                                <td>{row["Task Name"]}</td>
-                                <td>{row["Task Creation Date"]}</td>
-                                <td>{row["Task Assignment Date"]}</td>
-                                <td>{row["Task Assignee"]}</td>
-                                <td>{row["Task Progress"]}</td>
-                                <td>{row["Task Due Date"]}</td>
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-                <Outlet />
+
+            <div className="app">
+                {view === 'table' && (
+                    <>
+
+                        <Table rows={rows} deleteRow={handleDeleteRow} editRow={handleEditRow} />
+
+                        {modalOpen && (
+                            <Modal
+                                closeModal={() => {
+                                    setModalOpen(false);
+                                    setRowToEdit(null);
+                                }}
+                                onSubmit={handleSubmit}
+                                defaultValue={rowToEdit !== null && rows[rowToEdit]}
+                            />
+                        )}
+
+                        <Outlet />
+                    </>
+                )}
+                {view === 'userRegistration' && (
+
+                    <UserRegistration></UserRegistration>
+
+
+                )}
             </div>
+
+
             <nav className="nav">
                 {role === 'ROLE_ADMIN' && (
                     <ul>
                         <li>
-                            <Link to="/userregistration">User Registration</Link>
+                            <Link onClick={() => setView('userRegistration')} >User Registration </Link>
                         </li>
                         <li>
-                            <Link to="/taskmanagement">Task Management</Link>
+                            <Link onClick={() => setView('table')} >Task Management</Link>
                         </li>
                         <li>
                             <Link to="/leaderboard">LeaderBoard</Link>
@@ -96,11 +130,8 @@ const Dashboard = () => {
                         </li>
                     </ul>
                 )}
-
             </nav>
-
-            <Outlet />
-        </>
+        </div>
     )
 };
 
