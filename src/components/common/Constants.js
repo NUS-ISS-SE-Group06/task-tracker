6b7a14a67e285167b5db90dcef2cf63a17c41294
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2024-05-04 01:52:53 +0800

    NUSISSGRP6-32: Removed port for the prod URL

--- src/components/common/Constants.js
+++ src/components/common/Constants.js
@@ -1,3 +1,3 @@
 // constants.js
-export const BASE_URL = 'http://128.199.134.155:8688/api'; // Your base URL
+export const BASE_URL = 'http://128.199.134.155/api'; // Your base URL
 export const HOME_URL = 'http://localhost:3000/';
