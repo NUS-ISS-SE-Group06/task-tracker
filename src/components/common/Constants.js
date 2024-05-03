2ab62ad3b35d5552bed86011fbed63f3001e8bbe
sivabalaji1986 <42265473+sivabalaji1986@users.noreply.github.com>
2024-05-03 19:27:54 +0800

    NUSISSGRP6-32: Trying with nginx url

--- src/components/common/Constants.js
+++ src/components/common/Constants.js
@@ -1,3 +1,3 @@
 // constants.js
-export const BASE_URL = 'http://139.59.232.88:8688/api'; // Your base URL
+export const BASE_URL = 'http://139.59.232.88/api'; // Your base URL
 export const HOME_URL = 'http://localhost:3000/';
